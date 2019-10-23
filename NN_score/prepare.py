14197e03f9277030e9391309acc55d423839e4a2
nadiasemita <nadyusha.meshkova.00@mail.ru>
2019-10-23 13:27:58 +0300

    add prepare

--- NN_score/prepare.py
+++ NN_score/prepare.py
@@ -1,3 +1,29 @@
 # -*- coding: utf-8 -*-
+from pickle import load
+import random
+
+
+global_result = set()
+pairs = set()
+molecules = {}
+for number in range(1, 10000):
+    tuples = load(open('/home/nadia/data/triples/{}.pickle'.format(number), 'rb'))
+    local_result = set()
+    for _ in range(1000):
+        if len(local_result) == 34:
+            break
+        ml_random = random.choice(tuples)
+        sig = bytes(ml_random)
+        a = ml_random[0]
+        b = ml_random[1]
+        if ml_random in global_result or ml_random in local_result:
+            continue
+        if (b, a) in pairs:
+            continue
+        global_result.add(ml_random)
+        pairs.add((a, b))
+        local_result.add(ml_random)
+        molecules['sig'] = 'ml_random'
+print(molecules)
 
 # put there code for preparing data for modeling
