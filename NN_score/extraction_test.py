5ecae9e78cca86931e54b9d711ca5aec782a138d
nadiasemita <nadyusha.meshkova.00@mail.ru>
2019-12-14 15:13:45 +0300

    new code2.

--- NN_score/extraction_test.py
+++ NN_score/extraction_test.py
@@ -6,11 +6,13 @@ global_result = set()
 test, train, doubles = set(), set(), set()
 sig_test, sig_train = set(), set()
 NUMBER2, NUMBER3 = {0}, {0}
-logging.basicConfig(filename="sample.log", level=logging.INFO)
+# logging.basicConfig(filename="log.log", level=logging.INFO)
 logger = logging.getLogger("my test")
 logger.setLevel(logging.INFO)
-fh = logging.FileHandler("sample.log")
+fh = logging.FileHandler("log.log")
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
 
 for numeric in range(0, 4000):
@@ -28,25 +30,21 @@ for numeric in range(0, 4000):
         global_result.add(TUPLE)
         if len(test) == 1000:
             NUMBER2.add(max(NUMBER2) + 1)
-            with open('/home/nadia/data/test/{}test.pickle'.format(max(NUMBER2) + 1), 'wb') as f:
+            with open('/home/nadia/data/test/{}test.pickle'.format(max(NUMBER2)), 'wb') as f:
                 dump(test, f)
-            fh.setFormatter(formatter)
-            logger.addHandler(fh)
-            logger.info(f"test : {(len(NUMBER2)+1)*1000}")
+            logger.info(f" test : {(len(NUMBER2)+1)*1000}")
             test.clear()
         if numeric == 4000 and take_ml == tuples[-1]:
             NUMBER2.add(max(NUMBER2) + 1)
-            with open('/home/nadia/data/test/{}test.pickle'.format(max(NUMBER2)+1), 'wb') as f:
+            with open('/home/nadia/data/test/{}test.pickle'.format(max(NUMBER2)), 'wb') as f:
                 dump(test, f)
                 test.clear()
-with open('/home/nadia/data/sig_test/sig_test.pickle') as f:
+with open('/home/nadia/data/sig_test/sig_test.pickle', 'wb') as f:
     dump(sig_test, f)
-fh.setFormatter(formatter)
-logger.addHandler(fh)
 logger.info(f"All test unloaded. Number of files test : {len(NUMBER2)+1}")
 
 
-for numeric in range(4000, 10000):
+for numeric in range(4000, 43492):
     tuples = load(open('/home/nadia/data/True_pairs_new/{}.pickle'.format(numeric), 'rb'))
     for take_ml in tuples:
         a = take_ml[0]
@@ -57,22 +55,22 @@ for numeric in range(4000, 10000):
         if TUPLE in global_result or (sig_b, sig_a, take_ml[2], take_ml[3]) in global_result:
             continue
         if sig_a in sig_test or sig_b in sig_test:
-                doubles.add(TUPLE)
+            doubles.add(TUPLE)
         else:
             train.add(take_ml)
         global_result.add(TUPLE)
         if len(train) == 8000:
             NUMBER3.add(max(NUMBER3) + 1)
-            with open('/home/nadia/data/train/{}train.pickle'.format(NUMBER3), 'wb') as f:
+            with open('/home/nadia/data/train/{}train.pickle'.format(max(NUMBER3)), 'wb') as f:
                 dump(train, f)
-            fh.setFormatter(formatter)
-            logger.addHandler(fh)
-            logger.info(f"train : {(len(NUMBER3)+1)*8000}")
+            logger.info(f" train : {(len(NUMBER3)+1)*8000}")
             train.clear()
-        if numeric == 10000 and take_ml == tuples[-1]:
+        if numeric == 43491 and take_ml == tuples[-1]:
             NUMBER3.add(max(NUMBER3) + 1)
-            with open('/home/nadia/data/train/{}train.pickle', 'wb') as f:
+            with open('/home/nadia/data/train/{}train.pickle'.format(max(NUMBER3)), 'wb') as f:
                 dump(train, f)
             train.clear()
+with open('/home/nadia/data/test/doubles.pickle', 'wb') as f:
+    dump(doubles, f)
 logger.info(f"Number of train files for 8000 tuples : {len(NUMBER3)+1}. Number of test files for 1000 molecules : {len(NUMBER2)+1}. Number of doubles : {len(doubles)}")
 print('10000 pickles processed')
--- NN_score/test.py
+++ NN_score/test.py
@@ -1,17 +1,22 @@
 from pickle import load, dump
-import random
-from CGRtools.files import SDFwrite
-from CGRtools.files import SDFread
+import logging
 
 
 global_result, doubles = set(), set()
 NUMBER, NUMBER2, NUMBER3 = {0}, {0}, {0}
 train, test, validation = [], [], []
 sig_train, sig_test = set(), set()
+# logging.basicConfig(filename="ancient.log", level=logging.INFO)
+logger = logging.getLogger("my test")
+logger.setLevel(logging.INFO)
+fh = logging.FileHandler("ancient.log")
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
 
 end = False
-for numeric in range(0, 10000):
+for numeric in range(0, 43492):
     tuples = load(open('/home/nadia/data/True_pairs_new/{}.pickle'.format(numeric), 'rb'))  # открываю файл
     for take_ml in tuples:
         a = take_ml[0]
@@ -31,7 +36,8 @@ for numeric in range(0, 10000):
             else:
                 train.append(take_ml)
                 sig_train.update((sig_a, sig_b))
-        # если же а и б не встречались ранее в трайн, то добавляю в тест, вал или трайн (если ранее мы не добовляли в тест) в зависимости от их колличества
+        # если же а и б не встречались ранее в трайн, то добавляю в тест, вал или трайн
+        # (если ранее мы не добовляли в тест) в зависимости от их колличества
         else:
             if (sig_a in sig_test or sig_b in sig_test) or len(test)*8 < len(train):
                 if len(validation) < len(test):
@@ -46,33 +52,50 @@ for numeric in range(0, 10000):
 
         global_result.add(TUPLE)  # добавляю тюпл в глобал
         if len(validation) == 1000:
-            print('Intersection', len(sig_train.intersection(sig_test)))
             NUMBER.add(max(NUMBER) + 1)
+            with open('/home/nadia/data/validation2/{}validation.pickle'.format(max(NUMBER)), 'wb') as f:
+                dump(validation, f)
             validation = []
             NUMBER2.add(max(NUMBER2) + 1)
+            with open('/home/nadia/data/test2/{}test.pickle'.format(max(NUMBER2)), 'wb') as f:
+                dump(test, f)
             test = []
             NUMBER3.add(max(NUMBER3) + 1)
+            with open('/home/nadia/data/train2/{}train.pickle'.format(max(NUMBER3)), 'wb') as f:
+                dump(train[:8000], f)
             train = train[8000:]
-            print('train:', (len(NUMBER3)-1)*8000, ' test:', (len(NUMBER2)-1)*1000,
-                  ' validation:', (len(NUMBER)-1)*1000, ' doubles:', len(doubles))
+            logger.info(f" train : {max(NUMBER3)*8000},  test : {max(NUMBER2)*1000},"
+                        f" validation : {max(NUMBER)*1000}, doubles : {len(doubles)}")
         # если длина трайн достигла 8000, то сгружаем трайн полностью
         if len(train) == 16000:
             NUMBER3.add(max(NUMBER3) + 1)
+            with open('/home/nadia/data/train2/{}train.pickle'.format(max(NUMBER3)), 'wb') as f:
+                dump(train[8000:], f)
             train = train[:8000]
-            print('train:', (len(NUMBER3)-1)*8000, ' test:', (len(NUMBER2)-1)*1000,
-                  ' validation:', (len(NUMBER)-1)*1000, ' doubles:', len(doubles))
+            logger.info(f" train : {max(NUMBER3)*8000},  test : {max(NUMBER2)*1000},"
+                        f" validation : {max(NUMBER)*1000}, doubles : {len(doubles)}")
         # когда дошли до последнего тюпла последней молекулы все сгружаем в файл
         if numeric == 43491 and take_ml == tuples[-1]:
             NUMBER3.add(max(NUMBER3) + 1)
             NUMBER2.add(max(NUMBER2) + 1)
             NUMBER.add(max(NUMBER) + 1)
-            print('train:', (len(NUMBER3)-2)*8000+len(train), ' test:', (len(NUMBER2)-2)*1000+len(test),
-                  ' validation:', (len(NUMBER)-2)*1000+len(validation))
+            with open('/home/nadia/data/train2/{}train.pickle'.format(max(NUMBER3)), 'wb') as f:
+                dump(train, f)
+            with open('/home/nadia/data/test2/{}test.pickle'.format(max(NUMBER2)), 'wb') as f:
+                dump(test, f)
+            with open('/home/nadia/data/validation2/{}validation.pickle'.format(max(NUMBER)), 'wb') as f:
+                dump(validation, f)
+            logger.info(f" train : {(max(NUMBER3)-1)*8000 + len(train)},  test : {(max(NUMBER2)-1)*1000 + len(test)},"
+                        f" validation : {(max(NUMBER)-1)*1000 + len(validation)}, doubles : {len(doubles)}")
             train.clear()
             test.clear()
             validation.clear()
+with open('/home/nadia/data/test2/doubles.pickle', 'wb') as f:
+    dump(doubles, f)
 print(len(doubles))
-
+print('train files:', max(NUMBER3), ' test files:', max(NUMBER2),
+      'validation files:', max(NUMBER)*1000, 'doubles:', len(doubles))
+print('Intersection', len(sig_train.intersection(sig_test)))
 
 
 
